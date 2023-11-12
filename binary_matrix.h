d5010fe96ad139aef273123c17b11908ad214f49
Олексій Грушковський <oleksiy_grushkovskiy@danwin1210.de>
2023-11-12 02:04:16 +0200

    Створив бібліотеку для бінарних матриць, перевірив їх роботу в main

---
+++ binary_matrix.h
@@ -0,0 +1,16 @@
+typedef struct
+{
+    unsigned rows;
+    unsigned columns;
+    char *matrix;
+} BinaryMatrix;
+
+
+BinaryMatrix* BinaryMatrix_Create(unsigned rows, unsigned columns);
+void BinaryMatrix_Delete(BinaryMatrix* binary_matrix);
+BinaryMatrix* BinaryMatrix_CreateUnitMatrix(const int size);
+void BinaryMatrix_Print(BinaryMatrix* binary_matrix);
+BinaryMatrix* BinaryMatrix_MultiplyMatrices(BinaryMatrix* first_matrix, BinaryMatrix* second_matrix);
+int BinaryMatrix_Transpose(BinaryMatrix* binary_matrix);
+BinaryMatrix* BinaryMatrix_CombineMatrices(BinaryMatrix* first_matrix, BinaryMatrix* second_matrix);
+void BinaryMatrix_ImportFromArray(BinaryMatrix* binary_matrix, int* source, size_t size);
